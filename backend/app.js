c4d56a2630f6b2751e805537134da6cec2f9556d
ivedantsharma <vedantsharma102004@gmail.com>
2024-05-01 22:23:16 +0530

    deployed file

--- backend/app.js
+++ backend/app.js
@@ -7,14 +7,15 @@ const app = express();
 require("dotenv").config();
 
 const PORT = process.env.PORT;
-
+const BASE_URL =
+  "https://expense-backend-7blhr5qpv-ivedantsharmas-projects.vercel.app/";
 //middlewares
 app.use(express.json());
 app.use(cors());
 
 //routes
 readdirSync("./routes").map((route) =>
-  app.use("/api/v1", require("./routes/" + route))
+  app.use(`${BASE_URL}/api/v1`, require("./routes/" + route))
 );
 
 const server = () => {
